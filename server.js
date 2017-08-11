27965d9f2145dbb1c27049f6bb8b104a5d92426f
ARUNPRASAAD <30749644+ARUNPRASAAD@users.noreply.github.com>
2017-08-11 21:56:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var htmltemplate=`
 <!doctype html>
     <html>
         <head>
-        ${title}
+            <title>${title}</title>
             <style>
                 table{
                    border:1px solid red;
