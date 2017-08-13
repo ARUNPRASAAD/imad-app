11a348692f2b22e585e993bb82b2b0595dcb7cce
ARUNPRASAAD <30749644+ARUNPRASAAD@users.noreply.github.com>
2017-08-13 22:14:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+ //var counter=0;
+app.get('/counter', function(req,res){
+   
+   // counter=counter+1;
+    res.send(test);
+    
+});
+
 var articles={
         'article-one' :{
         title : 'arun-one',
@@ -89,13 +97,7 @@ app.get('/:articleName', function(req,res) {
   // var articleName='article-one';
     res.send(myfunction(articles[articleName]));
 });
- //var counter=0;
-app.get('/counter', function(req,res){
-   
-   // counter=counter+1;
-    res.send(test);
-    
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
