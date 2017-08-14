1425cd40c907ddd3ccd6e1b74504ed18d64a5531
ARUNPRASAAD <30749644+ARUNPRASAAD@users.noreply.github.com>
2017-08-14 11:59:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
 console.log('Loaded!');
+ var button= document.getElementById("but");
+            button.onclick = function(){
+
+              
+              var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange=function(){
+       // if (xhttp.readyState === XML.HttpRequest.DONE) {
+           // if(xhttp.status === 200){
+           if (this.readyState == 4 && this.status == 200){
+
+                         var counter=xhttp.responseText;
+              var counte=document.getElementById("count");
+
+              counte.innerHTML=counter.toString();
+           }
+          //  }
+      // }
+    }
+    xhttp.open("GET", "http://arunprasaadc.imad.hasura-app.io/counter",true);
+    xhttp.send(null);
+     
+};
+              
