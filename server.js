054763067ba3307f98416f9b882d0ff2ec83cf5f
ARUNPRASAAD <30749644+ARUNPRASAAD@users.noreply.github.com>
2017-08-14 19:33:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/counter', function(req,res){
     
 });
 var names=[];
-app.get('/submit/:names',function(req,res){
-    var name= req.params.names;
+app.get('/submit?name=',function(req,res){
+    var name= req.query.names;
     names.push(name);
     res.send(JSON.stringify(names));
     
